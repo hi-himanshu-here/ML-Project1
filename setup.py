c6e6dd0b21bd80fc338b77c0a60b8a0c62f791d0
hi-himanshu-here <himanshu.official765@gmail.com>
2025-01-03 11:42:25 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+# For build your application as a package itself.
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip() != HYPEN_E_DOT]
+
+    return requirements
+
+setup(
+name = 'Project1',
+version = '0.0.1',
+author = 'Himanshu',
+author_email = 'himanshu.official765@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
