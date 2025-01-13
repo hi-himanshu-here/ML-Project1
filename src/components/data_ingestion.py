f162d687200e9445e8aa7e4dc61fe8f239241ea7
M Naveen Kumar <naveencoursesonly@gmail.com>
2025-01-13 19:24:09 +0530

    fixing bugs

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,3 @@
-# Reading data from any source
-
 import os
 import sys
 from src.exception import CustomException
@@ -27,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('/Users/naveenkumar/Desktop/Temp/ML-Project1/notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
